8f6c64df2dcbe17e16a213e2c47491dbcb711e92
root <root@localhost.localdomain>
2016-12-28 11:36:52 +0800

    add dns config file

--- dns-boot.sh
+++ dns-boot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN --name=cluster.dns.server --network=docker --ip=172.18.0.250 tumingjian/dnsmasq
+docker run -d -p 53:53/tcp -p 53:53/udp -v /opt/soft/docker/env/dns-etc:/mnt/dns-etc --env-file=/opt/soft/docker/env/dns.env --cap-add=NET_ADMIN --name=cluster.dns.server --network=docker-net --ip=172.18.0.250  tumingjian/dnsmasq
