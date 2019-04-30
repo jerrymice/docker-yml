98bb9f5ea1f9fd36cb8a695da4fa593c7bf18953
jerrymice <tumingjian@qq.com>
2019-04-30 16:37:24 +0800

    修改coredns下载地址

--- kubernetes/install/download-image.sh
+++ kubernetes/install/download-image.sh
@@ -6,8 +6,12 @@ base_dir=$(cd `dirname $0`; pwd)
 source $base_dir/k8s-image-list.sh --pull
 for source_image in $K8S_IMAGES; do
     image_part=`echo $source_image | awk -F '/' '{print \$2}'`
-    mirror_image=$mirror_google/$image_part
-    echo "正在拉取$mirror_image"
+    if [[ $image_part =~ "coredns" ]]; then 
+        mirror_image="coredns/$image_part"
+    else 
+        mirror_image=$mirror_google/$image_part
+        echo "正在拉取$mirror_image"
+    fi
     docker pull $mirror_image
     docker tag  $mirror_image $google/$image_part
 done
