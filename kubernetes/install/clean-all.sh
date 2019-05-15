076c2cd517cdea423ab15d59e6fe857fc720a604
root <root@localhost.localdomain>
2019-05-15 17:49:01 +0800

    修改测试脚本,增加还原设置重新安装kubernetes的脚本

---
+++ kubernetes/install/clean-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubeadm reset -f 
+rm -rf ~/.kube/
+yum remove -y kubeadm kubectl kubelet
+rm -rf node-cache-install
+rm -rf cache
--- kubernetes/test/test.sh
+++ kubernetes/test/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#启动tomcat pod ,如果本地没有tomcat镜像,那么将去下载tomcat镜像.等待时间会很长
+#kubectl apply -f tomcat.yaml
 #这是一个测试脚本用于测试kubectl是否可用
 #列出测试的tomcat
 kubectl get pods -o wide | grep tomcat
