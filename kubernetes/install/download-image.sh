9406586b3b67f329d3bdc44a85ea97bdfce0d546
root <root@localhost.localdomain>
2019-05-15 16:33:48 +0800

    修复安装脚本BUG

--- kubernetes/install/download-image.sh
+++ kubernetes/install/download-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #镜像站用户名
-mirror_google=mirrorgooglecontainers
+mirror=mirrorgooglecontainers
 google=k8s.gcr.io
 base_dir=$(cd `dirname $0`; pwd)
 source $base_dir/k8s-image-list.sh --pull
@@ -9,10 +9,13 @@ for source_image in $K8S_IMAGES; do
     if [[ $image_part =~ "coredns" ]]; then 
         mirror_image="coredns/$image_part"
     else 
-        mirror_image=$mirror_google/$image_part
+        mirror_image=$mirror/$image_part
         echo "正在拉取$mirror_image"
     fi
     docker pull $mirror_image
     docker tag  $mirror_image $google/$image_part
 done
-docker rmi `docker images | grep $mirror_image | awk -F ' ' '{print $1,$2}' OFS=':'`
+mirror=mirrorgooglecontainers
+docker rmi `docker images | grep $mirror | awk -F ' ' '{print $1,$2}' OFS=':'`
+mirror=coredns/
+docker rmi `docker images | grep $mirror | awk -F ' ' '{print $1,$2}' OFS=':'`
